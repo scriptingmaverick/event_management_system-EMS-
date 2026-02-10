21a45a3eedfa6a6e7208bfc568a59dd31983f60f
Karthik Dasari <karthikdasari@Karthiks-MacBook-Pro.local>
2026-02-10 11:21:18 +0530

    Implements update user details functionality

---
+++ server/src/requests/update_user.js
@@ -0,0 +1,26 @@
+import { sendFailure, sendSuccess } from "../utils.js";
+
+export const createQuery = (userDetails, body) => {
+  const query = [];
+  const values = [];
+  query.push("UPDATE users SET");
+  for (const [column, value] of Object.entries(body)) {
+    query.push(`${column} = ?,`);
+    values.push(value);
+  }
+  query.push(query.pop().slice(0, -1));
+  query.push(`WHERE user_id = ?`);
+  values.push(userDetails.user_id);
+  return [query.join(" "), values];
+};
+
+export const updateUsernameOn = (db, userDetails, body) => {
+  try {
+    const [query, values] = createQuery(userDetails, body);
+    db.prepare(query).run(...values);
+
+    return sendSuccess(`updated successfully`, 200);
+  } catch {
+    return sendFailure("Internal server error", 501);
+  }
+};
---
+++ server/test/requests/update_user_test.js
@@ -0,0 +1,53 @@
+import { describe, it } from "@std/testing";
+import { assertEquals } from "@std/assert";
+import { DatabaseSync } from "node:sqlite";
+import {
+  createQuery,
+  updateUsernameOn,
+} from "../../src/requests/update_user.js";
+
+describe("testing update user functionality", () => {
+  const db = new DatabaseSync(":memory:");
+  const data = { user_id: 1, username: "karthik" };
+  db.exec(
+    "create table users(user_id integer primary key autoincrement, email text unique not null,password text not null,username text not null);",
+  );
+  db.prepare("insert into users(email, username, password) values(?,?,?);")
+    .run("hello@gmail.com", "hello", "1234");
+  const { username } = data;
+
+  const userDetails = {
+    user_id: 1,
+    username: "hello",
+    email: "hello@gmail.com",
+  };
+  const updatedRecord = {
+    email: "hello@gmail.com",
+    password: "1234",
+    username: "karthik",
+    user_id: 1,
+  };
+  describe("testing update user name ", () => {
+    it("testing with valid user name", () => {
+      updateUsernameOn(db, userDetails, { username });
+      const dataInDb = db.prepare("select * from users")
+        .all();
+      assertEquals(dataInDb[0], updatedRecord);
+    });
+  });
+
+  it("checking internal server error", async () => {
+    const result = updateUsernameOn(undefined, userDetails, { username });
+    const response = await result.text();
+    assertEquals(response, "Internal server error");
+  });
+
+  it("testing create query", () => {
+    const body = { username: "karthik", email: "karthik@gmail.com" };
+    const [query, values] = createQuery(userDetails, body);
+    const expectedQuery =
+      `UPDATE users SET username = ?, email = ? WHERE user_id = ?`;
+    assertEquals(query, expectedQuery);
+    assertEquals(values, ["karthik", "karthik@gmail.com", 1])
+  });
+});
