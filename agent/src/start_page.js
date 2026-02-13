e0a5393c5b0ee3745cb1a31a75d5b9a4f67d3f70
Khasim Sheik <khasimsheik@Khasims-MacBook-Pro.local>
2026-02-13 11:43:42 +0530

    Moves utilities to utils file

---
+++ agent/src/start_page.js
@@ -0,0 +1,71 @@
+import { select } from "@inquirer/prompts";
+import { green, red } from "jsr:@std/internal@^1.0.12/styles";
+import { BASE_URL, displayHeaders, displayResponse } from "./utils.js";
+
+
+const login = async () => {
+  displayHeaders("LOGIN");
+  const email = prompt("Enter email: ");
+  const password = prompt("Enter password: ");
+
+  const headers = { "content-type": "application/json" };
+  const requestBody = JSON.stringify({ email, password });
+
+  const request = { method: "POST", headers, body: requestBody };
+  const res = await fetch(`${BASE_URL}login`, request);
+
+  return await res.json();
+};
+
+const signup = async () => {
+  displayHeaders("SIGN UP");
+
+  const username = prompt("Enter username: ");
+  const email = prompt("Enter email: ");
+  const password = prompt("Enter password: ");
+
+  const headers = { "content-type": "application/json" };
+  const requestBody = JSON.stringify({ username, email, password });
+
+  const request = { method: "POST", headers, body: requestBody };
+  const res = await fetch(`${BASE_URL}sign-up`, request);
+
+  return await res.json();
+};
+
+const routeOperation = async (option, response) => {
+  if (!response.success) return mainPage();
+
+  if (option !== signup) return response;
+
+  const loginResponse = await login();
+
+  if (loginResponse.success) return loginResponse;
+
+  await displayResponse(loginResponse);
+  return mainPage();
+};
+
+const mainPage = async () => {
+  while (true) {
+    console.clear();
+    const mode = await select({
+      message: "SELECT MODE",
+      choices: [
+        { "name": "LOGIN", "value": login },
+        { "name": "SIGNUP", "value": signup },
+        { "name": "EXIT", "value": Deno.exit },
+      ],
+    });
+
+    const response = await mode();
+    await displayResponse(response);
+    return await routeOperation(mode, response);
+  }
+};
+
+const main = async () => {
+  console.log(await mainPage());
+};
+
+main();
---
+++ agent/src/utils.js
@@ -0,0 +1,21 @@
+export const BASE_URL = "http://localhost:5050/";
+export const { columns: COLUMNS } = Deno.consoleSize();
+
+export const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
+
+export const formatHeaders = (msg) => {
+  const pad = " ".repeat((COLUMNS / 4) - msg.length);
+  return `${pad}==> \x1b[1m ${msg} \x1b[0m <==\n`;
+};
+
+export const displayHeaders = (msg) => {
+  console.clear();
+  console.log(formatHeaders(msg));
+};
+
+export const formatResponse = (msg, success) => success ? green(msg) : red(msg);
+
+export const displayResponse = async ({ data, success }) => {
+  console.log(formatResponse(data, success));
+  await sleep(1500);
+};
